54a93c3c38b77dd64e37a621fa0865862ecc1ff2
Samuel Felipe de Oliveira Costa <costasamuelf7@gmail.com>
2021-06-15 22:17:39 -0300

    aulas JavaScript

--- tadw/aula_javascript/script.js
+++ tadw/aula_javascript/script.js
@@ -1,13 +1,15 @@
 // tipagem dinâmica
 // entende sozinho o tipo
-var sam = 'Samuca';
-var idade = 17;
-var preco = 14.65;
+var sam = prompt("Digite seu nome: ")
+var idade = prompt("Digite sua idade: ")
+document.write('Olá, '+sam+'!')
+document.write('<br>'+ 'Você tem '+idade+' anos.')
+/* var preco = 14.65;
 // false (não False)
 // ou true (não True)
-var guga = false;
+var guga = false
 // pra debugar ajuda:
-alert('Oi '+sam+'!');
-alert('Tu tem '+17+' anos é??');
-document.write('A variável lógica diz: '+guga+'. ');
-document.write('A variável "preco" é igual a '+preco);
+alert('Oi '+sam+'!')
+alert('Tu tem '+17+' anos é??')
+document.write('A variável lógica diz: '+guga+'. ')
+document.write('A variável "preco" é igual a '+preco) */
